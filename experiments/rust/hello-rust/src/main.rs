caf4dfe5d9f251efd224702253b1497c559f1a17
michael-christen <mchristen96@gmail.com>
2023-11-05 18:07:10 -0800

    Add rust experiments

---
+++ experiments/rust/hello-rust/src/main.rs
@@ -0,0 +1,11 @@
+// use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
+
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    ferris_says::say(&message, width, &mut writer).unwrap();
+}
