514a81fb18bf9ba3dad65924dc55ec0bf32888b6
michael-christen <mchristen96@gmail.com>
2023-11-26 13:36:23 -0800

    Everything out of experiments

--- examples/basic/app/hello-greet.cc
+++ examples/basic/app/hello-greet.cc
@@ -1,4 +1,4 @@
-#include "examples/basic/main/hello-greet.h"
+#include "examples/basic/app/hello-greet.h"
 #include <string>
 
 std::string get_greet(const std::string& who) { return "Hello " + who; }
