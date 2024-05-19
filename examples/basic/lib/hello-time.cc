ca9e1f7f56e68a2f328b583ec8f763570637552e
michael-christen <mchristen96@gmail.com>
2024-05-18 19:23:01 -0700

    Simplify

--- examples/basic/lib/hello-time.cc
+++ examples/basic/lib/hello-time.cc
@@ -3,8 +3,6 @@
 #include <ctime>
 #include <iostream>
 
-#include <google/protobuf/util/json_util.h>
-
 void print_localtime() {
   std::time_t result = std::time(nullptr);
   std::cout << std::asctime(std::localtime(&result));
