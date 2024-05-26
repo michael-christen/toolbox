ac4525e7156f07eba6e061ab657602a7afcd46ba
michael-christen <mchristen96@gmail.com>
2024-05-26 13:47:32 -0700

    slim down

--- lint.sh
+++ lint.sh
@@ -2,28 +2,6 @@
 
 set -euo pipefail
 
-REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
-
-# Find all Bazel-ish files - these templates come from Buildifier's default search list
-BAZEL_FILES=$(find ${REPO_ROOT} -type f \
-            \(   -name "*.bzl" \
-              -o -name "*.sky" \
-              -o -name "BUILD.bazel" \
-              -o -name "BUILD" \
-              -o -name "*.BUILD" \
-              -o -name "BUILD.*.bazel" \
-              -o -name "BUILD.*.oss" \
-              -o -name "MODULE.bazel" \
-              -o -name "WORKSPACE" \
-              -o -name "WORKSPACE.bazel" \
-              -o -name "WORKSPACE.oss" \
-              -o -name "WORKSPACE.*.bazel" \
-              -o -name "WORKSPACE.*.oss" \) \
-              -print)
-
-# Find all Markdown-ish files
-MARKDOWN_FILES=$(find ${REPO_ROOT} -type f -name "*.md" -print)
-
 # Function to print usage
 print_usage() {
   echo "Usage: $0 --mode [check|format]"
@@ -63,20 +41,33 @@ while true; do
   esac
 done
 
-# Update ARGS based on input
-ISORT_ARGS=("${REPO_ROOT}" "--dont-follow-links")
-BLACK_ARGS=("${REPO_ROOT}")
-FLAKE8_ARGS=("${REPO_ROOT}")
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
+
+# Find all Bazel-ish files - these templates come from Buildifier's default search list
+BAZEL_FILES=$(find ${REPO_ROOT} -type f \
+            \(   -name "*.bzl" \
+              -o -name "*.sky" \
+              -o -name "BUILD.bazel" \
+              -o -name "BUILD" \
+              -o -name "*.BUILD" \
+              -o -name "BUILD.*.bazel" \
+              -o -name "BUILD.*.oss" \
+              -o -name "MODULE.bazel" \
+              -o -name "WORKSPACE" \
+              -o -name "WORKSPACE.bazel" \
+              -o -name "WORKSPACE.oss" \
+              -o -name "WORKSPACE.*.bazel" \
+              -o -name "WORKSPACE.*.oss" \) \
+              -print)
+
+# Find all Markdown-ish files
+MARKDOWN_FILES=$(find ${REPO_ROOT} -type f -name "*.md" -print)
 
 # Check if the flag was set
 if [ -z "$mode" ]; then
   print_usage
 elif [ "$mode" = "check" ]; then
-  # XXX: lint off?
   BUILDIFIER_ARGS=("-lint=off" "-mode=check" "-v=false")
-  ISORT_ARGS+=("--check")
-  BLACK_ARGS+=("--check")
-  # flake8 doesn't format
   PRETTIER_ARGS=("--check" "--config ${REPO_ROOT}/.prettierrc")
   BAZEL_TOOL="//tools:check"
 elif [ "$mode" = "format" ]; then
@@ -84,6 +75,7 @@ elif [ "$mode" = "format" ]; then
   PRETTIER_ARGS=("--write" "--config ${REPO_ROOT}/.prettierrc")
   BAZEL_TOOL="//tools:format"
 fi
+PRETTIER_INVOCATION=""
 
 
 function print_error {
@@ -93,35 +85,15 @@ function print_error {
 }
 trap print_error ERR
 
-#####################
-# Bazel file linting
-#####################
-BUILDIFIER_INVOCATION="bazel run --config quiet -- //tools/buildifier ${BUILDIFIER_ARGS[@]}"
-echo $BAZEL_FILES | xargs ${BUILDIFIER_INVOCATION}
-
-
-#################
-# Python linting
-#################
-# Sort imports
-bazel run --config quiet  -- //tools/isort ${ISORT_ARGS[@]}
-# Autoformat
-bazel run --config quiet -- //tools/black ${BLACK_ARGS[@]}
-# Ensure flake8 compliance
-bazel run --config quiet  -- //tools/flake8 ${FLAKE8_ARGS[@]}
-
-# XXX: Replace most with this
-# bazel run --config quiet ${BAZEL_TOOL}
-
-#################
-# Markdown Linting
-#################
-PRETTIER_INVOCATION="bazel run --config quiet -- //tools/prettier ${PRETTIER_ARGS[@]}"
-echo $MARKDOWN_FILES | xargs ${PRETTIER_INVOCATION}
+CONFIG="--config quiet"
+# Can uncomment to get more verbose
+# CONFIG=""
+echo $BAZEL_FILES | xargs bazel run ${CONFIG} -- //tools/buildifier ${BUILDIFIER_ARGS[@]}
+bazel run ${CONFIG} -- ${BAZEL_TOOL}
+echo $MARKDOWN_FILES | xargs bazel run ${CONFIG} -- //tools/prettier ${PRETTIER_ARGS[@]}
 
 
 printf "\n✨ Linting completed successfully! ✨\n"
 
 # Add git grep XXX?
 # And --check option
-#
