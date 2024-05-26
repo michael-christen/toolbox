f3b2630c33b357858116844b2aa97cdc57ac60bc
michael-christen <mchristen96@gmail.com>
2024-05-25 23:40:35 -0700

    First pass

--- bzl/pytest_main.py
+++ bzl/pytest_main.py
@@ -1,7 +1,30 @@
+import os
 import sys
 
 import pytest
 
 
+def main():
+    pytest_args = [
+        "--ignore=external",
+        "-p", "no:cacheprovider",
+    ]
+
+    # https://bazel.build/reference/test-encyclopedia
+    test_tempdir = os.environ.get("TEST_TMPDIR", None)
+    if test_tempdir:
+        test_tempdir = os.path.join(os.path.abspath(test_tempdir), "pytest_temp")
+        pytest_args.append(f"--basetemp={test_tempdir}")
+
+    # https://bazel.build/docs/user-manual#flag--test_filter
+    # Take --test_filter options
+    test_filter = os.environ.get("TESTBRIDGE_TEST_ONLY", None)
+    if test_filter:
+        pytest_args.extend(["-k", test_filter])
+
+    # Invoke
+    sys.exit(pytest.main(pytest_args + sys.argv[1:]))
+
+
 if __name__ == '__main__':
-    sys.exit(pytest.main(sys.argv[1:]))
+    main()
