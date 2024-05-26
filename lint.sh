718631010b25b852fe86e618470a3281842601e8
michael-christen <mchristen96@gmail.com>
2024-05-26 13:31:32 -0700

    improve

--- lint.sh
+++ lint.sh
@@ -4,17 +4,6 @@ set -euo pipefail
 
 REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
 
-function print_error {
-    read line file <<<$(caller)
-    printf "\n⛔️ An error occurred during the following lint step ⛔️\n" >&2
-    sed "${line}q;d" "$file" >&2
-}
-trap print_error ERR
-
-
-#####################
-# Bazel file linting
-#####################
 # Find all Bazel-ish files - these templates come from Buildifier's default search list
 BAZEL_FILES=$(find ${REPO_ROOT} -type f \
             \(   -name "*.bzl" \
@@ -31,30 +20,108 @@ BAZEL_FILES=$(find ${REPO_ROOT} -type f \
               -o -name "WORKSPACE.*.bazel" \
               -o -name "WORKSPACE.*.oss" \) \
               -print)
-BUILDIFIER_ARGS=("-lint=fix" "-mode=fix" "-v=false")
-BUILDIFIER_INVOCATION="bazel run -- //tools/buildifier ${BUILDIFIER_ARGS[@]}"
+
+# Find all Markdown-ish files
+MARKDOWN_FILES=$(find ${REPO_ROOT} -type f -name "*.md" -print)
+
+# Function to print usage
+print_usage() {
+  echo "Usage: $0 --mode [check|format]"
+  exit 1
+}
+
+# Parse arguments using getopt
+OPTS=$(getopt -o '' -l 'mode:' -- "$@")
+if [ $? != 0 ]; then
+  print_usage
+fi
+eval set -- "$OPTS"
+
+# Initialize the flag
+mode=
+
+while true; do
+  case "$1" in
+    --mode)
+      if [ "$2" = "check" ]; then
+        mode=$2
+      elif [ "$2" = "format" ]; then
+        mode=$2
+      else
+        echo "Invalid value for --mode. Use 'check' or 'format'."
+        exit 1
+      fi
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      print_usage
+      ;;
+  esac
+done
+
+# Update ARGS based on input
+ISORT_ARGS=("${REPO_ROOT}" "--dont-follow-links")
+BLACK_ARGS=("${REPO_ROOT}")
+FLAKE8_ARGS=("${REPO_ROOT}")
+
+# Check if the flag was set
+if [ -z "$mode" ]; then
+  print_usage
+elif [ "$mode" = "check" ]; then
+  # XXX: lint off?
+  BUILDIFIER_ARGS=("-lint=off" "-mode=check" "-v=false")
+  ISORT_ARGS+=("--check")
+  BLACK_ARGS+=("--check")
+  # flake8 doesn't format
+  PRETTIER_ARGS=("--check" "--config ${REPO_ROOT}/.prettierrc")
+  BAZEL_TOOL="//tools:check"
+elif [ "$mode" = "format" ]; then
+  BUILDIFIER_ARGS=("-lint=fix" "-mode=fix" "-v=false")
+  PRETTIER_ARGS=("--write" "--config ${REPO_ROOT}/.prettierrc")
+  BAZEL_TOOL="//tools:format"
+fi
+
+
+function print_error {
+    read line file <<<$(caller)
+    printf "\n⛔️ An error occurred during the following lint step ⛔️\n" >&2
+    sed "${line}q;d" "$file" >&2
+}
+trap print_error ERR
+
+#####################
+# Bazel file linting
+#####################
+BUILDIFIER_INVOCATION="bazel run --config quiet -- //tools/buildifier ${BUILDIFIER_ARGS[@]}"
 echo $BAZEL_FILES | xargs ${BUILDIFIER_INVOCATION}
+
 
 #################
 # Python linting
 #################
 # Sort imports
-# XXX: --check-only
-bazel run -- //tools/isort ${REPO_ROOT} --dont-follow-links
+bazel run --config quiet  -- //tools/isort ${ISORT_ARGS[@]}
 # Autoformat
-bazel run -- //tools/black ${REPO_ROOT}
+bazel run --config quiet -- //tools/black ${BLACK_ARGS[@]}
 # Ensure flake8 compliance
-bazel run -- //tools/flake8 ${REPO_ROOT}
+bazel run --config quiet  -- //tools/flake8 ${FLAKE8_ARGS[@]}
+
+# XXX: Replace most with this
+# bazel run --config quiet ${BAZEL_TOOL}
 
 #################
 # Markdown Linting
 #################
-MARKDOWN_FILES=$(find ${REPO_ROOT} -type f -name "*.md" -print)
-PRETTIER_ARGS=("--write" "--config ${REPO_ROOT}/.prettierrc")
-PRETTIER_INVOCATION="bazel run -- //tools/prettier ${PRETTIER_ARGS[@]}"
+PRETTIER_INVOCATION="bazel run --config quiet -- //tools/prettier ${PRETTIER_ARGS[@]}"
 echo $MARKDOWN_FILES | xargs ${PRETTIER_INVOCATION}
 
 
 printf "\n✨ Linting completed successfully! ✨\n"
 
 # Add git grep XXX?
+# And --check option
+#
--- tools/flake8/flake8.py
+++ tools/flake8/flake8.py
@@ -1,4 +1,9 @@
+import sys
+
 from flake8.main import cli
 
+from tools import utils
+
 if __name__ == "__main__":
-    cli.main()
+    utils.update_argv_with_workspace()
+    sys.exit(cli.main())
