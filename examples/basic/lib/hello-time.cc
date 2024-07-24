186d54efe5a3fe27226ebfc7e004d1ec8da0cd01
michael-christen <mchristen96@gmail.com>
2024-07-23 21:51:35 -0700

    Remove my overrides

--- examples/basic/lib/hello-time.cc
+++ examples/basic/lib/hello-time.cc
@@ -3,8 +3,6 @@
 #include <ctime>
 #include <iostream>
 
-#include "pw_containers/vector.h"
-
 void print_localtime() {
   std::time_t result = std::time(nullptr);
   std::cout << std::asctime(std::localtime(&result));
