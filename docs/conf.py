d80cb2d2cbc6f2cad62e016c6dd8dcc1bd9c7e7b
michael-christen <mchristen96@gmail.com>
2024-08-18 20:21:38 -0700

    First docs

---
+++ docs/conf.py
@@ -0,0 +1,39 @@
+import sphinx_rtd_theme
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'toolbox'
+copyright = '2024, Michael Christen'
+author = 'Michael Christen'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx_rtd_theme',
+]
+
+exclude_patterns = []
+
+# `root_doc`: Provided at Bazel level
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
+html_static_path = ['_static']
+templates_path = ['_templates']
+
+# html_sidebars = {
+#     "**": [
+#         "navigation.html",
+#     ],
+# }
---
+++ docs/index.rst
@@ -0,0 +1,23 @@
+.. toolbox documentation master file, created by
+   sphinx-quickstart on Sun Aug 18 19:40:25 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+toolbox documentation
+=====================
+
+.. note::
+
+   This project is under active development.
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 4
+   :caption: Contents:
+
+   hello
+
