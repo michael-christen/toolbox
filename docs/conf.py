17609c82406c4a3a8bef964898ec0c9b2d847aaa
michael-christen <mchristen96@gmail.com>
2024-09-17 09:15:16 -0700

    keep em

--- docs/conf.py
+++ docs/conf.py
@@ -67,7 +67,7 @@ myst_enable_extensions = [
     "fieldlist",
     "html_admonition",
     "html_image",
-    # "linkify",
+    "linkify",
     "replacements",
     "smartquotes",
     "strikethrough",
