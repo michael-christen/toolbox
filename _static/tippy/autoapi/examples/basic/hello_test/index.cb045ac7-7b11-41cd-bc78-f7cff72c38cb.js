2b58783335f4dfeb0d4cffa9f27d830a964348de
michael-christen <michael-christen@users.noreply.github.com>
2024-10-29 06:21:49 +0000

    deploy: cff56b44780b1f3cec6276c611509a6888cf0809

---
+++ _static/tippy/autoapi/examples/basic/hello_test/index.cb045ac7-7b11-41cd-bc78-f7cff72c38cb.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"#module-contents\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Module Contents<a class=\"headerlink\" href=\"#module-contents\" title=\"Link to this heading\">\uf0c1</a></h2>", "a[href=\"#classes\"]": "<h2 class=\"tippy-header\" style=\"margin-top: 0;\">Classes<a class=\"headerlink\" href=\"#classes\" title=\"Link to this heading\">\uf0c1</a></h2>", "a[href=\"#module-examples.basic.hello_test\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">examples.basic.hello_test<a class=\"headerlink\" href=\"#module-examples.basic.hello_test\" title=\"Link to this heading\">\uf0c1</a></h1><h2>Classes<a class=\"headerlink\" href=\"#classes\" title=\"Link to this heading\">\uf0c1</a></h2>", "a[href=\"#examples.basic.hello_test.TestHello\"]": "<dt class=\"sig sig-object py\" id=\"examples.basic.hello_test.TestHello\">\n<em class=\"property\"><span class=\"pre\">class</span><span class=\"w\"> </span></em><span class=\"sig-prename descclassname\"><span class=\"pre\">examples.basic.hello_test.</span></span><span class=\"sig-name descname\"><span class=\"pre\">TestHello</span></span><span class=\"sig-paren\">(</span><em class=\"sig-param\"><span class=\"n\"><span class=\"pre\">methodName</span></span><span class=\"o\"><span class=\"pre\">=</span></span><span class=\"default_value\"><span class=\"pre\">'runTest'</span></span></em><span class=\"sig-paren\">)</span><a class=\"reference internal\" href=\"../../../../_modules/examples/basic/hello_test.html#TestHello\"><span class=\"viewcode-link\"><span class=\"pre\">[source]</span></span></a></dt><dd><p>Bases: <a class=\"reference external\" href=\"https://docs.python.org/3.10/library/unittest.html#unittest.TestCase\" title=\"(in Python v3.10)\"><code class=\"xref py py-obj docutils literal notranslate\"><span class=\"pre\">unittest.TestCase</span></code></a></p><p>Test proto loading.</p></dd>", "a[href=\"#examples.basic.hello_test.TestHello.test_basics\"]": "<dt class=\"sig sig-object py\" id=\"examples.basic.hello_test.TestHello.test_basics\">\n<span class=\"sig-name descname\"><span class=\"pre\">test_basics</span></span><span class=\"sig-paren\">(</span><span class=\"sig-paren\">)</span><a class=\"reference internal\" href=\"../../../../_modules/examples/basic/hello_test.html#TestHello.test_basics\"><span class=\"viewcode-link\"><span class=\"pre\">[source]</span></span></a></dt><dd></dd>"}
+skip_classes = ["headerlink", "sd-stretched-link"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(` ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
---
+++ _static/tippy/autoapi/index.0dd97720-abe5-4cf2-85b0-fac97c13c091.js
@@ -0,0 +1,22 @@
+selector_to_html = {"a[href=\"examples/basic/hello_pb2_grpc/index.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">examples.basic.hello_pb2_grpc<a class=\"headerlink\" href=\"#module-examples.basic.hello_pb2_grpc\" title=\"Link to this heading\">\uf0c1</a></h1><p>Client and server classes corresponding to protobuf-defined services.</p>", "a[href=\"examples/basic/hello_test/index.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">examples.basic.hello_test<a class=\"headerlink\" href=\"#module-examples.basic.hello_test\" title=\"Link to this heading\">\uf0c1</a></h1><h2>Classes<a class=\"headerlink\" href=\"#classes\" title=\"Link to this heading\">\uf0c1</a></h2>", "a[href=\"#api-reference\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">API Reference<a class=\"headerlink\" href=\"#api-reference\" title=\"Link to this heading\">\uf0c1</a></h1><p>This page contains auto-generated API reference documentation <a class=\"footnote-reference brackets\" href=\"#f1\" id=\"id1\" role=\"doc-noteref\"><span class=\"fn-bracket\">[</span>1<span class=\"fn-bracket\">]</span></a>.</p>", "a[href=\"#f1\"]": "<aside class=\"footnote brackets\" id=\"f1\" role=\"doc-footnote\">\n<span class=\"label\"><span class=\"fn-bracket\">[</span><a href=\"#id1\" role=\"doc-backlink\">1</a><span class=\"fn-bracket\">]</span></span>\n<p>Created with <a class=\"reference external\" href=\"https://github.com/readthedocs/sphinx-autoapi\">sphinx-autoapi</a></p>\n</aside>", "a[href=\"examples/basic/main/index.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">examples.basic.main<a class=\"headerlink\" href=\"#module-examples.basic.main\" title=\"Link to this heading\">\uf0c1</a></h1><h2>Functions<a class=\"headerlink\" href=\"#functions\" title=\"Link to this heading\">\uf0c1</a></h2>", "a[href=\"examples/basic/client/index.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">examples.basic.client<a class=\"headerlink\" href=\"#module-examples.basic.client\" title=\"Link to this heading\">\uf0c1</a></h1><h2>Functions<a class=\"headerlink\" href=\"#functions\" title=\"Link to this heading\">\uf0c1</a></h2>", "a[href=\"examples/basic/grpc_test/index.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">examples.basic.grpc_test<a class=\"headerlink\" href=\"#module-examples.basic.grpc_test\" title=\"Link to this heading\">\uf0c1</a></h1><h2>Classes<a class=\"headerlink\" href=\"#classes\" title=\"Link to this heading\">\uf0c1</a></h2>", "a[href=\"examples/basic/hello_pb2/index.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">examples.basic.hello_pb2<a class=\"headerlink\" href=\"#module-examples.basic.hello_pb2\" title=\"Link to this heading\">\uf0c1</a></h1><p>Generated protocol buffer code.</p>", "a[href=\"examples/basic/server/index.html\"]": "<h1 class=\"tippy-header\" style=\"margin-top: 0;\">examples.basic.server<a class=\"headerlink\" href=\"#module-examples.basic.server\" title=\"Link to this heading\">\uf0c1</a></h1><h2>Classes<a class=\"headerlink\" href=\"#classes\" title=\"Link to this heading\">\uf0c1</a></h2>"}
+skip_classes = ["headerlink", "sd-stretched-link"]
+
+window.onload = function () {
+    for (const [select, tip_html] of Object.entries(selector_to_html)) {
+        const links = document.querySelectorAll(` ${select}`);
+        for (const link of links) {
+            if (skip_classes.some(c => link.classList.contains(c))) {
+                continue;
+            }
+
+            tippy(link, {
+                content: tip_html,
+                allowHTML: true,
+                arrow: true,
+                placement: 'auto-start', maxWidth: 500, interactive: false,
+
+            });
+        };
+    };
+    console.log("tippy tips loaded!");
+};
