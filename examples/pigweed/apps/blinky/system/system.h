fa9157d6a1451153e09ee51818db62465fb594c1
Michael Christen <mchristen96@gmail.com>
2024-11-10 16:47:33 -0800

    Setup pigweed platform builds as a separate third_party folder for the most part (#141)
    Attempt to segregate most of the pigweed marked libraries in order to
    use them as the library they are / keep apache 2 in tact for them.

---
+++ examples/pigweed/apps/blinky/system/system.h
@@ -0,0 +1,31 @@
+// Copyright 2024 The Pigweed Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+#pragma once
+
+#include "pw_digital_io/digital_io.h"
+
+// The functions in this file return specific implementations of singleton types
+// provided by the system.
+
+namespace demo::system {
+
+/// Initializes the system. This must be called before anything else in `main`.
+void Init();
+
+/// Starts the main system scheduler. This function never returns.
+[[noreturn]] void Start();
+
+pw::digital_io::DigitalInOut& MonochromeLed();
+
+}  // namespace demo::system
