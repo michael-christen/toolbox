342133b0767059f2f2608146c79347bcc51435ae
michael-christen <mchristen96@gmail.com>
2024-11-11 22:25:35 -0800

    Fix and comment

--- third_party/pigweed/system/common_rp2_system.h
+++ third_party/pigweed/system/common_rp2_system.h
@@ -11,8 +11,20 @@ namespace system {
 // Initialize PICO_SDK
 void CommonRp2Init();
 
+// This is a macro that sets up a channel of specified size, an allocator, and
+// then starts the system.
+//
+// Example Usage:
+//
+/// #include "third_party/pigweed/system/common_rp2_system.h"
+///
+/// void Start() {
+///   // <insert custom code here, etc.>
+///
+///   PW_RP2_SYSTEM_START(2048)
+/// }
 #define PW_RP2_SYSTEM_START(channel_buffer_size)                     \
-  static std::byte channel_buffer[2048];                             \
+  static std::byte channel_buffer[channel_buffer_size];              \
   static pw::multibuf::SimpleAllocator multibuf_alloc(               \
       channel_buffer, pw::System().allocator());                     \
   pw::SystemStart(pw::channel::Rp2StdioChannelInit(multibuf_alloc)); \
