1e5662aaa16a77a330ffc775bb09d9b3bd079923
michael-christen <mchristen96@gmail.com>
2024-11-11 22:39:50 -0800

    lint

--- apps/sbr/system/rp2_system.cc
+++ apps/sbr/system/rp2_system.cc
@@ -1,11 +1,14 @@
 #include "apps/sbr/system/system.h"
 #include "third_party/pigweed/system/common_rp2_system.h"
 
-// XXX: Decide on namespace style
-namespace apps::sbr::system {
+namespace apps {
+namespace sbr {
+namespace system {
 
 void Init() { third_party::pigweed::system::CommonRp2Init(); }
 
 void Start() { PW_RP2_SYSTEM_START(2048) }
 
-}  // namespace apps::sbr::system
+}  // namespace system
+}  // namespace sbr
+}  // namespace apps
