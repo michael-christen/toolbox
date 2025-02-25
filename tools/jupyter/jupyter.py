29ffeeeba3fc7608b208513263fdfce486369fee
Michael Christen <mchristen96@gmail.com>
2025-02-25 06:17:12 -0800

    Tofupilot Example (#169)
    Run like:
    
    ```
    TOFUPILOT_API_KEY=<API_KEY> bazel run examples/tofupilot:tofupilot_example
    ```
    
    Example output:
    https://www.tofupilot.app/mchristen/runs/00767074-e1c2-11ef-9434-df71c4093031
    
    Separately, pinned setuptools to deal with space in file and bazel /
    rules_python not supporting it. While there, sorted requirements
    alphabetically

--- tools/jupyter/jupyter.py
+++ tools/jupyter/jupyter.py
@@ -1,12 +1,17 @@
-import os
-import re
-import sys
+import collections
+import collections.abc
 
-from notebook import app
+setattr(collections, "MutableMapping", collections.abc.MutableMapping)
+
+import os  # noqa: E402
+import re  # noqa: E402
+import sys  # noqa: E402
+
+from notebook import notebookapp  # noqa: E402
 
 if __name__ == "__main__":
     if "BUILD_WORKSPACE_DIRECTORY" in os.environ:
         os.chdir(os.environ["BUILD_WORKSPACE_DIRECTORY"])
     sys.argv[0] = re.sub(r"(-script\.pyw?|\.exe)?$", "", sys.argv[0])
     # XXX: Failing to start well
-    sys.exit(app.main())
+    sys.exit(notebookapp.main())
