f18f1030d8b0ed9ce438fd2f91cf8eb0381906f6
michael-christen <mchristen96@gmail.com>
2026-01-18 11:56:25 -0800

    Add gephi binary and update _multirun_under_cwd

--- tools/_multirun_under_cwd.sh
+++ tools/_multirun_under_cwd.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # From: https://blog.aspect.build/run-tools-installed-by-bazel
 # XXX: May be a bit outdated
-target="@multitool//tools/$(basename "$0")"
-bazel 2>/dev/null build "$target" && exec $(bazel info execution_root)/$(bazel 2>/dev/null cquery --output=files "$target") "$@"
+# target="@multitool//tools/$(basename "$0")"
+# bazel 2>/dev/null build "$target" && exec $(bazel info execution_root)/$(bazel 2>/dev/null cquery --output=files "$target") "$@"
+bazel run "@multitool//tools/$( basename $0 ):cwd" -- "$@"
