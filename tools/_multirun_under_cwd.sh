1dc1ae889d4d9c8445272184c18977052765affc
michael-christen <mchristen96@gmail.com>
2026-01-18 12:48:32 -0800

    update

--- tools/_multirun_under_cwd.sh
+++ tools/_multirun_under_cwd.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # From: https://blog.aspect.build/run-tools-installed-by-bazel
-# XXX: May be a bit outdated
-# target="@multitool//tools/$(basename "$0")"
-# bazel 2>/dev/null build "$target" && exec $(bazel info execution_root)/$(bazel 2>/dev/null cquery --output=files "$target") "$@"
+# Can also use `bazel run //tools:bazel_env` and the provided binaries with
+# direnv: https://blog.aspect.build/bazel-devenv
 bazel run "@multitool//tools/$( basename $0 ):cwd" -- "$@"
